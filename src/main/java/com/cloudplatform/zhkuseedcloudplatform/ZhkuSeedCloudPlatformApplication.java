8aacac601644e549d277e89ef58d1e1989fa101d
Faink <1951437547@qq.com>
2020-01-09 16:37:10 +0800

    111

---
+++ src/main/java/com/cloudplatform/zhkuseedcloudplatform/ZhkuSeedCloudPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cloudplatform.zhkuseedcloudplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhkuSeedCloudPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhkuSeedCloudPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudplatform/zhkuseedcloudplatform/ZhkuSeedCloudPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudplatform.zhkuseedcloudplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhkuSeedCloudPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
